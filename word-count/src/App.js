8b00118966cc5751914acf8cb3e0d3d58e204e87
CarinaLR <carinalr84@gmail.com>
2021-08-28 21:18:58 -0400

    new commit

---
+++ word-count/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>WORD COUNT</h1>
+      </header>
+      <div>Counts the number of words in a block of text.</div>
+    </div>
+  );
+}
+
+export default App;

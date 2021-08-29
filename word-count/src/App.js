98ac1840713bc0efb52e45db1d3387e1cd3e550b
CarinaLR <carinalr84@gmail.com>
2021-08-29 14:25:30 -0400

    add layout

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -9,12 +9,41 @@ function App() {
       </header>
       <div className="container">
         <div className="row">
-          <div class="col-12 col-sm-6 col-md-8">
-            1 of 2 Counts the number of words in a block of text.
-            <textarea></textarea>
+          <div className="col-12 col-sm-6 col-md-8">
+            <form>
+              <div className="container">
+                <h2>
+                  Place your text in the Textarea and submit your request.
+                </h2>
+                <div class="form-group">
+                  <label for="exampleFormControlTextarea1">
+                    Example textarea
+                  </label>
+                  <textarea
+                    className="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="3"
+                  ></textarea>
+                </div>
+              </div>
+            </form>
           </div>
-          <div class="col-6 col-md-4">
-            2 of 2 Counts the number of words in a block of text.
+          <div className="col-6 col-md-4">
+            <div>
+              <h2>Results will be shown below after your submission.</h2>
+              <div className="card">
+                <ul className="list-group list-group-flush">
+                  <label className="card-header">CHECK RESULT</label>
+                  <li className="list-group-item">Words Count: 0</li>
+                  <li className="list-group-item">Characters Count: 0</li>
+                  <li className="list-group-item">
+                    Characters Count without spacing: 0
+                  </li>
+                  <li className="list-group-item">Sentence Count: 0</li>
+                  <li className="list-group-item">Paragraph Count: 0</li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
       </div>

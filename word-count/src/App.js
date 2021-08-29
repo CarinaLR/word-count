2f8f4fb602aa487da842a9fdb91315244dd29c3b
CarinaLR <carinalr84@gmail.com>
2021-08-28 21:40:35 -0400

    working on layout

--- word-count/src/App.js
+++ word-count/src/App.js
@@ -7,7 +7,17 @@ function App() {
       <header className="App-header">
         <h1>WORD COUNT</h1>
       </header>
-      <div>Counts the number of words in a block of text.</div>
+      <div className="container">
+        <div className="row">
+          <div class="col-12 col-sm-6 col-md-8">
+            1 of 2 Counts the number of words in a block of text.
+            <textarea></textarea>
+          </div>
+          <div class="col-6 col-md-4">
+            2 of 2 Counts the number of words in a block of text.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

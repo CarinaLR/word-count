00b61f286f83e7d9213b3af99c5405a44b3db3f6
CarinaLR <carinalr84@gmail.com>
2021-09-16 13:12:19 -0400

    add comments

--- word-count/src/Hooks/useText.spec.js
+++ word-count/src/Hooks/useText.spec.js
@@ -1,9 +1,9 @@
 import { render, getByTestId } from "@testing-library/jest-dom";
 import App from "./App";
 
-//The useEffect Hook implementation is essentially setting the value of the words state to the localStorage.
-//Submitting a text via the input field changes the value of the words state.
-//The words state is saved in the localStorage.
+//The useEffect Hook implementation is essentially setting the value of the text state to the localStorage.
+//Submitting a text via the input field changes the value of the text state.
+//The text state is saved in the localStorage.
 
 it("App loads with initial state of text empty", () => {
   const { container } = render(<App />);

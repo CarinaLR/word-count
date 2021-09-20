11ca5f8e46f4d58206b49eeafcd1605d756a87c6
CarinaLR <carinalr84@gmail.com>
2021-09-20 15:19:29 -0400

    working on unicode chars

--- word-count/src/Hooks/useText.js
+++ word-count/src/Hooks/useText.js
@@ -1,4 +1,10 @@
 import { useState, useEffect } from "react";
+/* suggestion
+clear out all unicode characters.
+ -This approach uses a Regular Expression to remove the Non-ASCII characters from the string.
+ -Only characters that has value from zero to 127 are valid. (0x7F is 127 in hex).
+ -Use .replace() method to replace the Non-ASCII characters with the empty string.
+*/
 //custom hooks allow to break down and establish the logic them in a reusable way.
 const useText = (text) => {
   const [words, setWords] = useState();
@@ -22,6 +28,7 @@ const useText = (text) => {
   let charExcludingSpacesCount;
   let sentenceCount;
   let paragraphCount;
+  let str = text;
 
   if (text === " ") {
     wordsCount = 0;
@@ -39,6 +46,9 @@ const useText = (text) => {
     //paragraph counter
     paragraphCount = text.replace(/\n$/gm, "").split(/\n/).length;
     //words and character counter
+    // eslint-disable-next-line no-control-regex
+    str = str.replace(/[^\x00-\x7F]/g, "");
+    console.log(str);
     arrStr.forEach((c) => {
       if (arrStr.length === 1 && c !== " ") wordsCount = arrStr.length;
       if (arrStr.length === 2 && c === " ") {

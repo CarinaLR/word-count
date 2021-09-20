55855d0a6ddc9b5bc6c9db77bb78539f7aa22493
CarinaLR <carinalr84@gmail.com>
2021-09-20 15:24:36 -0400

    use regular expressions

--- word-count/src/Hooks/useText.js
+++ word-count/src/Hooks/useText.js
@@ -1,10 +1,5 @@
 import { useState, useEffect } from "react";
-/* suggestion
-clear out all unicode characters.
- -This approach uses a Regular Expression to remove the Non-ASCII characters from the string.
- -Only characters that has value from zero to 127 are valid. (0x7F is 127 in hex).
- -Use .replace() method to replace the Non-ASCII characters with the empty string.
-*/
+
 //custom hooks allow to break down and establish the logic them in a reusable way.
 const useText = (text) => {
   const [words, setWords] = useState();
@@ -37,7 +32,10 @@ const useText = (text) => {
     sentenceCount = 0;
     paragraphCount = 0;
   } else {
-    arrStr = text.split(" ");
+    // uses a Regular Expression to remove the Non-ASCII characters from the string. .replace() method to replace the Non-ASCII characters with the empty string.
+    // eslint-disable-next-line no-control-regex
+    str = str.replace(/[^\x00-\x7F]/g, "");
+    arrStr = str.split(" ");
     wordsCount = 0;
     charCount = 0;
     charExcludingSpacesCount = 0;
@@ -46,9 +44,6 @@ const useText = (text) => {
     //paragraph counter
     paragraphCount = text.replace(/\n$/gm, "").split(/\n/).length;
     //words and character counter
-    // eslint-disable-next-line no-control-regex
-    str = str.replace(/[^\x00-\x7F]/g, "");
-    console.log(str);
     arrStr.forEach((c) => {
       if (arrStr.length === 1 && c !== " ") wordsCount = arrStr.length;
       if (arrStr.length === 2 && c === " ") {

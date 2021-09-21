bd776765e4f6a0f4b0b0121df9c38c81571a546d
CarinaLR <carinalr84@gmail.com>
2021-09-21 14:55:15 -0400

    working on test

--- word-count/src/Hooks/useText.spec.js
+++ word-count/src/Hooks/useText.spec.js
@@ -1,12 +1,12 @@
-import { render, getByTestId } from "@testing-library/jest-dom";
-import App from "./App";
+import { render, getByTestId } from "testing-library/react";
+import TextBlog from "../components/TextBlog";
 
 //The useEffect Hook implementation is essentially setting the value of the text state to the localStorage.
 //Submitting a text via the input field changes the value of the text state.
 //The text state is saved in the localStorage.
 
 it("App receives a string input as text from the user.", () => {
-  const { container } = render(<App />);
+  const { container } = render(<TextBlog />);
   const text = getByTestId(container, "required-input");
   expect(text.textContent).toBe("");
 });
